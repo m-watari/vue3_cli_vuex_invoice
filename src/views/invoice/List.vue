b08c75a9a18d57342a3f707c93d877461a14310f
m-watari <watari141@gmail.com>
2022-12-19 23:17:36 +0900

    invoice一覧表示

--- src/views/invoice/List.vue
+++ src/views/invoice/List.vue
@@ -5,7 +5,12 @@
     <div>
       <router-link to="/invoice-create">create</router-link>
     </div>
-  <div>list</div>
+  <div>
+    <!-- invice_listをループ表示 -->
+    <div v-for="invoice in invoice_list" :key="invoice.id">
+      <router-link :to="'/invoice-detail/' + invoice.id">{{ invoice.title }}</router-link>
+    </div>
+  </div>
   </div>
   <FooterView/>
 </template>
@@ -20,13 +25,24 @@ export default {
     HeaderView,
     FooterView
   },
-  // data() {
-  //   return {
-  //   }
-  // },
-  // methods: {
-    
-  // },
+  data() {
+    return {
+      invoice_list: []
+    }
+  },
+  methods: {
+    // invoiceをDBから取得
+    getInvoiceList() {
+      this.$axios.get('/api/invoice')
+        .then((response) => {
+          this.invoice_list = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+
+  },
   mounted () {
     console.log('NODE_ENV', process.env.NODE_ENV)
   }
